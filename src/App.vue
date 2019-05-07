ff69ffaca3f455f06fca92be3518425b26adfa72
endiko <kogay.nadezhda@gmail.com>
2019-05-07 17:17:01 +0500

    Changed template and styles

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,69 @@
 <template>
   <v-app>
-    <v-navigation-drawer temporary app v-model="sideNav">
-      <v-list>
-        <v-list-tile v-for="(item, index) in menuItems" :key="index" router :to="item.link">
-          <v-list-tile-action>
-            <v-icon>{{item.icon}}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>{{item.title}}</v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile v-if="userIsAuthenticated" @click="onLogout">
-          <v-list-tile-action>
-            <v-icon>exit_to_app</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>Выйти</v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar dark class="cyan lighten-3">
-      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav" class="hidden-md-and-up"></v-toolbar-side-icon>
-      <v-toolbar-title>
-        <router-link to="/" tag="span" style="cursor: pointer"></router-link>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat v-for="(item, index) in menuItems" :key="index" router :to="item.link">
-          <v-icon left dark>{{item.icon}}</v-icon>
-          {{item.title}}
-        </v-btn>
-        <v-btn flat v-if="userIsAuthenticated" @click="onLogout">
-          <v-icon left dark>exit_to_app</v-icon>Выйти
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-    <main>
+    <main class="main">
+      <v-navigation-drawer temporary app v-model="sideNav">
+        <v-list>
+          <v-list-tile v-for="(item, index) in menuItems" :key="index" router :to="item.link">
+            <v-list-tile-action>
+              <v-icon>{{item.icon}}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>{{item.title}}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="userIsAuthenticated" @click="onLogout">
+            <v-list-tile-action>
+              <v-icon>exit_to_app</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>Выйти</v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar dark color="#e57373">
+        <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav" class="hidden-md-and-up"></v-toolbar-side-icon>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          <svg-base icon-name="logo" viewBox="0 0 97.502 97.502" class="logo">
+            <logo-svg/>
+          </svg-base>
+        </router-link>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn flat v-for="(item, index) in menuItems" :key="index" router :to="item.link">
+            <v-icon left dark>{{item.icon}}</v-icon>
+            {{item.title}}
+          </v-btn>
+          <v-btn flat v-if="userIsAuthenticated" @click="onLogout">
+            <v-icon left dark>exit_to_app</v-icon>Выйти
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
       <v-layout row wrap mt-4 v-if="!userIsAuthenticated">
         <v-flex xs12 sm6 class="text-xs-center text-sm-right">
-          <v-btn
-            flat
-            large
-            round
-            router
-            to="/meetups"
-            color="cyan darken-3"
-            style="border: 1px solid"
-          >Все встречи</v-btn>
+          <v-btn medium router to="/meetups" color="#527965" class="white--text">
+            <v-icon left light>people_outline</v-icon>Все встречи
+          </v-btn>
         </v-flex>
         <v-flex xs12 sm6 class="text-xs-center text-sm-left">
-          <v-btn
-            flat
-            large
-            round
-            router
-            to="/meetups/new"
-            color="cyan darken-3"
-            style="border: 1px solid"
-          >Создать встречу</v-btn>
+          <v-btn medium router to="/meetups/new" color="#527965" class="white--text">
+            <v-icon left light>create</v-icon>Создать встречу
+          </v-btn>
         </v-flex>
       </v-layout>
       <router-view></router-view>
     </main>
+    <v-footer class="footer">
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import svgIcon from './components/SvgIcon.vue'
+import svgBase from './components/SvgBase.vue'
+import logoSvg from './components/LogoSvg.vue'
+
 export default {
   name: 'App',
   components: {
-    svgIcon
+    svgBase,
+    logoSvg
   },
   data() {
     return {
@@ -119,12 +115,27 @@ export default {
   methods: {
     onLogout() {
       this.$store.dispatch('logout')
+      this.$router.push('/meetups')
     }
   }
 }
 </script>
 
 <style lang="scss">
+.main {
+  min-height: 100%;
+  position: relative;
+  overflow: hidden;
+  &:after {
+    content: '';
+    display: block;
+    height: 36px;
+  }
+}
+.footer {
+  margin-top: -36px;
+}
+
 .custom-loader {
   animation: loader 1s infinite;
   display: flex;
@@ -161,4 +172,9 @@ export default {
     transform: rotate(360deg);
   }
 }
+
+// .logo {
+//   width: 64px;
+//   height: auto;
+// }
 </style>
