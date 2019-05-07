cdd082bfca37233990eeec3059479e80b15a4a59
endiko <kogay.nadezhda@gmail.com>
2019-05-07 17:24:02 +0500

    Fixed view data in template and styles

--- src/components/Meetup/Meetup.vue
+++ src/components/Meetup/Meetup.vue
@@ -5,32 +5,30 @@
         <v-progress-circular indeterminate color="primary" :width="7" :size="70"></v-progress-circular>
       </v-flex>
     </v-layout>
-    <v-layout row wrap v-else>
+    <v-layout row wrap v-else mb-5>
       <v-flex xs12>
-        <v-card>
-          <v-card-title>
-            <h4 class="primary--text">{{meetup.title}}</h4>
-            <template v-if="userIsCreator">
-              <v-spacer></v-spacer>
-              <app-edit-meetup-details-dialog :meetup="meetup"></app-edit-meetup-details-dialog>
-            </template>
-          </v-card-title>
-          <v-img :src="meetup.imageUrl" height="400px"></v-img>
-
-          <v-card-text>
-            <!-- <div>{{meetup.date}} в {{meetup.time}}</div> -->
-
-            <div>Место проведения: {{meetup.location}} в {{meetup.fullDate}}</div>
-            <app-edit-meetup-date-dialog :meetup="meetup" v-if="userIsCreator">Редактировать дату</app-edit-meetup-date-dialog>
-
-            <p>{{meetup.description}}</p>
-          </v-card-text>
-          <v-card-actions>
+        <v-layout row wrap align-center mt-5 mb-4>
+          <h1>{{meetup.title}}</h1>
+          <template v-if="userIsCreator">
             <v-spacer></v-spacer>
-            <!-- <v-btn flat class="primary" @click.stop="dialog = true">Зарегистрироваться</v-btn> -->
-            <register-dialog :meetupId="meetup.id" v-if="userIsAuthenticated"></register-dialog>
-          </v-card-actions>
-        </v-card>
+            <app-edit-meetup-details-dialog :meetup="meetup"></app-edit-meetup-details-dialog>
+          </template>
+        </v-layout>
+        <v-img :src="meetup.imageUrl" height="500px"></v-img>
+        <v-layout row mt-5>
+          <div>{{meetup.description}}</div>
+        </v-layout>
+        <v-layout
+          row
+          mt-4
+          style="font-size: 16px; font-weight: 300"
+        >Место проведения: {{meetup.location}}</v-layout>
+        <v-layout row style="font-size: 16px; font-weight: 300" mt-1 mb-3>Время: {{meetup.fullDate}}</v-layout>
+
+        <v-layout row mt-2>
+          <v-spacer></v-spacer>
+          <register-dialog :meetupId="meetup.id" v-if="userIsAuthenticated"></register-dialog>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -70,4 +68,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+span {
+  font-size: 16px;
+  font-weight: 300;
+}
 </style>
