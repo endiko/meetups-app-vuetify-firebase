b7583aa0b0fd9d77950990faebbc406b75833fa8
endiko <kogay.nadezhda@gmail.com>
2019-05-07 17:22:53 +0500

    Change template and view

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,25 +1,24 @@
 <template>
   <v-container>
-    <v-layout row wrap mb-3 v-for="item in meetups" :key="item.id">
-      <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
-        <v-card class="info">
+    <v-layout row wrap justify-space-around>
+      <v-flex v-for="item in meetups" :key="item.id" xs12 mb-4>
+        <v-card color="white">
           <v-container fluid>
-            <v-layout row>
-              <v-flex xs5 sm4 md3>
-                <v-img :src="item.imageUrl" height="130px"></v-img>
+            <v-layout row wrap justify-space-between>
+              <v-flex xs12 sm12 md6 lg6>
+                <v-img :src="item.imageUrl" height="100%"></v-img>
               </v-flex>
 
-              <v-flex xs7 sm8 md9>
-                <v-card-title primary-title>
-                  <div>
-                    <h3 class="headline mb-0">{{item.title}}</h3>
-                    <div>{{item.fullDate}}</div>
-                  </div>
-                </v-card-title>
+              <v-flex xs12 sm12 md6 lg6 style="padding-left: 30px">
+                <v-card-title primary-title class="headline">{{item.title}}</v-card-title>
+                <v-card-text>
+                  <div>{{item.fullDate}}</div>
+                  <div>{{item.description | truncate(150)}}</div>
+                </v-card-text>
 
                 <v-card-actions>
-                  <v-btn flat color="white" :to="'/meetups/' + item.id">
-                    <v-icon left light>arrow_forward</v-icon>Просмотреть
+                  <v-btn medium color="#527965" class="white--text" :to="'/meetups/' + item.id">
+                    <v-icon left light>remove_red_eye</v-icon>Просмотреть
                   </v-btn>
                 </v-card-actions>
               </v-flex>
