c420a4c7b475a6f7a3fd2dfa72eff8a03ed0eb20
endiko <kogay.nadezhda@gmail.com>
2019-05-07 17:19:51 +0500

    Add filter

---
+++ src/filters/date.js
@@ -0,0 +1,9 @@
+export default date => {
+  return new Date(date).toLocaleDateString('ru-RU', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  })
+}
