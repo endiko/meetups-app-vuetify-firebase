0b37ac2c2d7b9b2b50c4a7be352a8d02b88da99c
endiko <kogay.nadezhda@gmail.com>
2019-05-17 22:03:26 +0500

    Change table's items layout in template

--- src/components/User/Profile.vue
+++ src/components/User/Profile.vue
@@ -15,18 +15,26 @@
         <table v-if="getUsersCreatedMeetups.length > 0">
           <thead>
             <tr>
-              <td>Встреча</td>
-              <td>Место встречи</td>
-              <td>Краткое описание</td>
-              <td>Дата встречи</td>
+              <th>Встреча</th>
+              <th>Место встречи</th>
+              <th>Краткое описание</th>
+              <th>Дата встречи</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="meetup in getUsersCreatedMeetups" :key="meetup.id">
-              <td>{{meetup.title}}</td>
-              <td>{{meetup.location}}</td>
-              <td>{{meetup.description | truncate(100)}}</td>
-              <td>{{meetup.fullDate | date}}</td>
+              <td data-th="Встреча">
+                <span>{{meetup.title}}</span>
+              </td>
+              <td data-th="Место встречи">
+                <span>{{meetup.location}}</span>
+              </td>
+              <td data-th="Краткое описание" class="table__desc">
+                <span>{{meetup.description | truncate(100)}}</span>
+              </td>
+              <td data-th="Дата встречи">
+                <span>{{meetup.fullDate | date}}</span>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -43,18 +51,26 @@
         <table v-if="getUsersRegisteredMeetups.length > 0">
           <thead>
             <tr>
-              <td>Встреча</td>
-              <td>Место встречи</td>
-              <td>Краткое описание</td>
-              <td>Дата встречи</td>
+              <th>Встреча</th>
+              <th>Место встречи</th>
+              <th>Краткое описание</th>
+              <th>Дата встречи</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="meetup in getUsersRegisteredMeetups" :key="meetup.id">
-              <td>{{meetup.title}}</td>
-              <td>{{meetup.location}}</td>
-              <td>{{meetup.description | truncate(100)}}</td>
-              <td>{{meetup.fullDate | date}}</td>
+              <td data-th="Встреча">
+                <span>{{meetup.title}}</span>
+              </td>
+              <td data-th="Место встречи">
+                <span>{{meetup.location}}</span>
+              </td>
+              <td data-th="Краткое описание" class="table__desc">
+                <span>{{meetup.description | truncate(100)}}</span>
+              </td>
+              <td data-th="Дата встречи">
+                <span>{{meetup.fullDate | date}}</span>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -91,23 +107,70 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.headline {
+  @media all and (max-width: 768px) {
+    font-size: 22px !important;
+  }
+}
 h3 {
   font-size: 20px;
   font-weight: 400;
   color: #af4448;
 }
+
 table {
+  display: inline-block;
+  max-width: 100%;
+  overflow-x: auto;
   border-collapse: collapse;
 }
+
+.table__desc {
+  min-width: 400px;
+  @media all and (max-width: 768px) {
+    min-width: 300px;
+  }
+}
+
 thead {
   background: #f8ffd7;
   font-weight: 700;
+}
+th {
+  padding-top: 15px;
+  padding-bottom: 15px;
+  // @media all and (max-width: 768px) {
+  //   display: none;
+  // }
 }
 tr {
   border-bottom: 1px solid #c5e1a5;
 }
 td {
-  padding: 20px 10px;
-  width: 25%;
+  padding: 1.8rem 1rem;
+  min-width: 200px;
+  @media all and (max-width: 768px) {
+    padding: 1.2rem 0.8rem;
+    min-width: 145px;
+  }
+  // @media all and (max-width: 768px) {
+  //   display: block;
+  //   width: 100%;
+  //   padding: 0;
+  //   margin-bottom: 1.5rem;
+  //   &:before {
+  //     content: attr(data-th) ': ';
+  //     font-weight: bold;
+  //     display: block;
+  //     background: #f8ffd7;
+  //     border-bottom: 1px solid #c5e1a5;
+  //     padding: 1.2rem 0.7rem;
+  //   }
+
+  //   span {
+  //     display: inline-block;
+  //     padding: 1.2rem 0.7rem;
+  //   }
+  // }
 }
 </style>
